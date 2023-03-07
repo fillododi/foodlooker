834bebd47756d84d70ab364c678b76b41d5a8ad4
fillododi <fillo.dodi@gmail.com>
2023-03-07 17:05:54 +0100

    Finished dish overview on main menu Added styles Added data fetching

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,9 +1,32 @@
 import { useGlobalContext } from "../context"
+import {BsHandThumbsUp} from "react-icons/bs";
 
 const Dishes = () => {
-    const context = useGlobalContext();
-    console.log(context);
+    const {loading, dishes} = useGlobalContext();
 
-    return("Dishes")
+    if(loading){
+        return <section className="section">
+            <h4>Loading...</h4>
+        </section>
+    }
+
+    if (dishes.length < 1){
+        return <section className="section">
+            <h4>No dishes found</h4>
+        </section>
+    }
+
+    return <section className="section-center">
+        {dishes.map((dish)=>{
+            const {idMeal: dish_id, strMeal: dish_name, strMealThumb: dish_image} = dish
+            return <article className="dish" key={dish_id}>
+                <img src={dish_image} className="img" />
+                <footer>
+                    <h5>{dish_name}</h5>
+                    <button className="like-btn"><BsHandThumbsUp></BsHandThumbsUp></button>
+                </footer>
+            </article>
+        })}
+    </section>
 }
 export default Dishes
--- src/context.jsx
+++ src/context.jsx
@@ -1,10 +1,36 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    const [loading, setLoading] = useState(false)
+    const [dishes, setDishes] = useState([])
+
+    const allDishUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+    const randomDishUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
+    const fetchDishes = async (url) => {
+        setLoading(true)
+        try{
+
+            const response = await fetch(url)
+            const data = await response.json()
+            if(data.meals){
+                setDishes(data.meals)
+            } else {
+                setDishes([])
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        setLoading(false)
+    }
+
+    useEffect(()=>{
+        fetchDishes(allDishUrl)
+    }, [])
+
     return (
-        <AppContext.Provider value={{name: 'lasagne', origin: 'italy'}}>
+        <AppContext.Provider value={{loading, dishes}}>
             {children}
         </AppContext.Provider>
     )
